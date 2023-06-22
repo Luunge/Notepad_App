37ae18969437289a0c114dd045365faff4bd9995
Abdirahman Ahmad <115547577+Luunge@users.noreply.github.com>
2023-06-22 20:44:21 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Notepad_App
-{               
+{              
     public partial class Form1 : Form
     {
         public Form1()
